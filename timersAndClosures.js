920ef51fb87f0082def8e9f260cabcdb47a13039
PJ Mantoss <46089979+PJMantoss@users.noreply.github.com>
2020-02-21 14:41:28 +0100

    add new challenge

--- timersAndClosures.js
+++ timersAndClosures.js
@@ -154,3 +154,11 @@ function findIndex(arr, fn){
 
 //test
 findIndex([8,11,4,27], function(val){return val >= 10}); // 1
+
+/*Ex. 10
+Write a function called specialMultiply which accepts two parameters. 
+If the function is passed both parameters, it should return the product of the two. 
+If the function is only passed one parameter - it should return a function which can later be 
+passed another parameter to return the product. 
+You will have to use closure and arguments to solve this.
+*/
